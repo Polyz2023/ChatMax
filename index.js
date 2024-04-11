ebc8187e7f3a2509c6bcb4d7bb5930ec90d21c41
MaksStudio <maxrognov23@gmail.com>
2024-04-11 08:15:40 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io"); // Import the Server class from socket.io
+const io = new Server(server); // Initialize Socket.IO with the server
+
+server.listen(4007, () => {
+  console.log('Server is running on http://localhost:4007');
+});
+
+app.get('/', (req, res) => {
+    // Make sure the path to your index.html is correct. If it's in the same directory, this will work.
+    res.sendFile(__dirname + '/index.html');
+});
+
+var users = [];
+var connections = [];
+
+// Socket.IO server side connection
+io.on('connection', (socket) => {
+    connections.push(socket);
+    console.log("On");
+
+    socket.on('disconnect', (data) => {
+        connections.splice(connections.indexOf(socket), 1);
+        console.log("Off");
+    });
+
+    socket.on('send mess', (data) => {
+        console.log("Received message:", data); // Добавляем это для отладки
+        io.sockets.emit('add mess', {msg: data.mess, name: data.name});
+    });
+});
